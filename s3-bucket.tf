c3d81dc70402e42fbb8e8318b82e78f8c0e20155
Wilson Firmino <wil.lira.1986@gmail.com>
2023-03-18 21:02:43 +0000

    Added s3 bucket module

---
+++ s3-bucket.tf
@@ -0,0 +1,11 @@
+module "s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+
+  bucket = "my-s3-bucket"
+  acl    = "private"
+  bucket_prefix = var.prefix
+  versioning = {
+    enabled = true
+  }
+
+}
